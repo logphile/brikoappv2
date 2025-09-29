c33da0dd5935076b9e7d514c71957863062647a7
Phil <phil@logphile.com>
2025-09-29 05:00:26 -0400

    feat: add color legend to step header and simplify page layout

--- app/components/guide/StepPage.vue
+++ app/components/guide/StepPage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import StepHeader from './StepHeader.vue'
-import ActiveColorStrip from './ActiveColorStrip.vue'
 import LegendCompass from './LegendCompass.vue'
 
 defineProps<{
@@ -17,14 +16,11 @@ defineExpose({ canvasRef })
 </script>
 
 <template>
-  <section class="grid grid-cols-[1fr_260px] gap-6">
-    <div class="flex flex-col gap-4">
-      <StepHeader :step="step" :placedNow="placedNow" :totalPlaced="totalPlaced" />
-      <div class="rounded-2xl border border-[#2F3061]/30 bg-white p-2">
-        <canvas ref="canvasRef" class="w-full h-auto rounded-xl bg-[#F5F4F1]"></canvas>
-      </div>
-      <LegendCompass />
+  <section class="flex flex-col gap-4">
+    <StepHeader :step="step" :placedNow="placedNow" :totalPlaced="totalPlaced" :activeColors="activeColors" />
+    <div class="rounded-2xl border border-[#2F3061]/30 bg-white p-2">
+      <canvas ref="canvasRef" class="w-full h-auto rounded-xl bg-[#F5F4F1]"></canvas>
     </div>
-    <ActiveColorStrip :rows="activeColors" />
+    <LegendCompass />
   </section>
 </template>
