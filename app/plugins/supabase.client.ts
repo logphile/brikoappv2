42a23d8ed2bdda90a390ae6de770dcfffe2d1a04
Phil <phil@logphile.com>
2025-10-07 19:52:27 -0400

    refactor: migrate to @nuxtjs/supabase auto-imported composables and standardize border radius classes

--- app/plugins/supabase.client.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from 'nuxt/app'
-
-// No-op: rely entirely on @nuxtjs/supabase composables (useSupabaseClient/useSupabaseUser)
-export default defineNuxtPlugin(() => {
-  return {}
-})
