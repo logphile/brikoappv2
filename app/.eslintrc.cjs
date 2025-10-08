2374b6ab276af44d03c5e0ba6fc07f4445d48d7d
Phil <phil@logphile.com>
2025-10-08 17:34:11 -0400

    chore: add ESLint rules to prevent direct public asset imports and refactor UploadBox icon usage

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -7,6 +7,25 @@ module.exports = {
       svg: 'always',
       math: 'always'
     }],
-    'vue/no-parsing-error': 'error'
+    'vue/no-parsing-error': 'error',
+    // Guardrails: prevent importing public assets in scripts and leading-slash URL module patterns
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "ImportDeclaration[source.value=/^\\/.+\\.(svg|png|jpe?g|webp|gif)$/]",
+        message:
+          "Don't import public files in <script>. Use '/file.ext' (public) or assets with ?url.",
+      },
+      {
+        selector: "NewExpression[callee.name='URL'][arguments.0.value=/^\\//]",
+        message:
+          "Avoid new URL('/file.ext', import.meta.url). Use a public URL string or assets ?url.",
+      },
+      {
+        selector: "CallExpression[callee.object.name='URL'][arguments.0.value=/^\\//]",
+        message:
+          "Avoid URL('/file.ext', import.meta.url). Use a public URL string or assets ?url.",
+      }
+    ]
   }
 }
