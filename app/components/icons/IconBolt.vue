610edb4e4ae44d0270afafebdcf46d3335446bc9
Phil <phil@logphile.com>
2025-10-11 13:33:59 -0400

    refactor: replace unplugin-icons with local SVG components for better performance

---
+++ app/components/icons/IconBolt.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="currentColor" class="inline-block">
+    <!-- simple, bold lightning bolt that reads well at 20px -->
+    <path d="M13 2 4 14h7l-1 8 10-12h-7l0-8z"/>
+  </svg>
+</template>
---
+++ app/components/icons/IconReceiptLong.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
+       stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+       class="inline-block">
+    <path d="M6 2h10l2 2v18l-3-2-3 2-3-2-3 2V4a2 2 0 0 1 2-2z"/>
+    <path d="M9 8h6M9 12h6M9 16h4"/>
+  </svg>
+</template>
