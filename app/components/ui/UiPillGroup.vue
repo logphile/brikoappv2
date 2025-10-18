ed01edd0ccf7cc12b07f9880b0904115ed56adc2
Phil <phil@logphile.com>
2025-10-18 18:23:28 -0400

    feat(ui): add UiPillGroup for filters/segmented controls

---
+++ app/components/ui/UiPillGroup.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  options: { label: string; value: string }[]
+  modelValue: string
+  dense?: boolean
+}>()
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="inline-flex gap-2">
+    <button
+      v-for="opt in options" :key="opt.value"
+      class="rounded-xl px-3"
+      :class="[
+        props.dense ? 'h-8 text-sm' : 'h-9 text-sm',
+        opt.value === props.modelValue
+          ? 'bg-pink text-white'
+          : 'ring-1 ring-black/10 bg-white/70 text-black hover:bg-white'
+      ]"
+      @click="emit('update:modelValue', opt.value)"
+    >{{ opt.label }}</button>
+  </div>
+</template>
